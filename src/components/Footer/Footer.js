8988879a37b9da6a7f26506c1abc4f2125d48071
joynal-uddin <joynaluddinbhuiyan2@.com>
2020-08-27 22:45:02 +0600

    this is online course assignment commit

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './Footer.css'
+const Footer = () => {
+    return (
+        <div className="footer">
+            &copy; Joynal Uddin Bhuiyan | 2020
+        </div>
+    );
+};
+
+export default Footer;
